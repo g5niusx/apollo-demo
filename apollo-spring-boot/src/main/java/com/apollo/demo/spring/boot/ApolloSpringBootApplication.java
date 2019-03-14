51d999c230e103ea2fa33beb7199e16369e12486
g5niusx <g5niusx@gmail.com>
2019-03-14 19:03:32 +0800

    :sparkles: 增加 spring boot 配置

--- apollo-spring-boot/src/main/java/com/apollo/demo/spring/boot/ApolloSpringBootApplication.java
+++ apollo-spring-boot/src/main/java/com/apollo/demo/spring/boot/ApolloSpringBootApplication.java
@@ -1,13 +1,33 @@
 package com.apollo.demo.spring.boot;
 
+import com.apollo.demo.spring.boot.properties.App;
+import com.ctrip.framework.apollo.model.ConfigChange;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfigChangeListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.scope.refresh.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class ApolloSpringBootApplication {
+
+    public static final String APP = "app";
+
+    @Value("redis.host")
+    private String test;
+
+    @Autowired
+    private RefreshScope refreshScope;
+
+    @Autowired
+    private App app;
 
     public static void main(String[] args) {
         SpringApplication.run(ApolloSpringBootApplication.class, args);
@@ -16,7 +36,28 @@ public class ApolloSpringBootApplication {
 
     @GetMapping("test")
     public String test() {
+        return test;
+    }
 
-        return "test";
+    /**
+     * 测试从远程直接加载bean
+     *
+     * @return
+     */
+    @GetMapping("app")
+    public String app() {
+        return app.toString();
+    }
+
+    @ApolloConfigChangeListener
+    public void listener(ConfigChangeEvent configChangeEvent) {
+        configChangeEvent.changedKeys().forEach(key -> {
+            ConfigChange change = configChangeEvent.getChange(key);
+            log.info("key [{}] changed [{}] ---> [{}]", key, change.getOldValue(), change.getNewValue());
+            if (change.getPropertyName().startsWith("app")) {
+                // 刷新 app 这个bean的值
+                refreshScope.refresh(APP);
+            }
+        });
     }
 }
---
+++ apollo-spring-boot/src/main/java/com/apollo/demo/spring/boot/properties/App.java
@@ -0,0 +1,22 @@
+package com.apollo.demo.spring.boot.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+import static com.apollo.demo.spring.boot.ApolloSpringBootApplication.APP;
+
+/**
+ * 通过远程直接注入bean对象
+ */
+@Data
+@ConfigurationProperties(prefix = "app")
+@RefreshScope
+@Component(APP)
+public class App {
+
+    private String name;
+    private String host;
+    private String port;
+}
